5c8f46892c87df172a6bfc29b41e216d03a40a2b
Aderauwu04 <aderabg1@gmail.com>
2023-03-14 21:49:07 -0400

    Creada sección de registro y inicio de sesión

--- netcli/tailwind.config.cjs
+++ netcli/tailwind.config.cjs
@@ -3,8 +3,22 @@ module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
+		colors: {
+			'transparent': 'transparent',
+			'black': '#000',
+			'white': '#fff',
+			'muted': {
+				DEFAULT:'#f5f5f5',
+				neutral: '#a1a1aa',
+			},
+			'primario': {
+				DEFAULT: '#CC0812',
+				dark: '#8C060C',
+				light: '#E50914',
+			}
+		},
 	},
 	plugins: [
-		// require('@tailwindcss/forms'),
+		require('@tailwindcss/forms'),
 	],
 };
